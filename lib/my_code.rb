05d2064f4cc3bfd1261691927ca135f69cd5b942
David Van Fleet <dvfleet413@gmail.com>
2019-12-21 17:56:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
 # Your Code Here
+def map(array)
+  result = []
+  i = 0 
+  while i < array.length do
+    # Lightbulb moment --> yield EXECUTES the code in the block, it's kind of like a variable
+    result << yield(array[i])
+    i += 1 
+  end
+  result
+end
+
+
+def reduce(array, starting_point = 0)
+  total = 0
+  i = 0 
+  while i < array.length do
+    yield
+    
+    i += 1 
+  end
+  total
+end
+
